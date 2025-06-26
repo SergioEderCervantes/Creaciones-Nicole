35c2d4366c6c86447e5990acc58788c00d768fc6
dany Carabez <al267187@edu.uaa.mx>
2025-06-26 13:25:07 -0600

    products modify

--- backend/app/routes/products.py
+++ backend/app/routes/products.py
@@ -16,11 +16,28 @@ def get_sections() -> Response:
     return jsonify(response)
 
 @api_bp.route('/products/<section>', methods=['GET','POST'])
-def section_products(section: str) -> str:
+def section_products(section: str) -> Response:
+    prodRepo = GenericRepository(ProductsModel, ProductSchema)
+    catRepo = GenericRepository(CategoryModel, CategorySchema)
+    
+    category = catRepo.get_by_field('name', section)
+    
+    if not category:
+        return jsonify({"error": "Categoria no encontrada"}), 404
+    
     if request.method == "GET":
-        return f"<h1> get Products of section {section} working and i love it </h1>"
-    else:
-        return f" post Products of section {section} working "
+        products = prodRepo.get_by_field('category_id', category.id)
+        products_schema = prodRepo.to_schema(products)
+        response = {
+            "data": [producto.model_dump() for product in products_schema]
+        }
+        return jsonify(response)
+    elif request.method == "POST":
+        data = request.json
+        
+        data['category_id'] = category.id
+        new_product = prodRepo.create(data)
+        return jsonify({"data": new_product.model_dump()}), 201
         
 @api_bp.route('/products/<section>/<id>', methods=['GET', 'PATCH', 'DELETE'])
 def get_product_by_id(section:str, id:int) -> str:
