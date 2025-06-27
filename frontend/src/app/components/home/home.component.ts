78f17b80e697595104b3e6c8f6726ae59b13ba2e
StoneyGravy963 <al210356@edu.uaa.mx>
2025-06-26 20:08:02 -0600

    arreglando ando

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent],
+  imports: [HeroComponent,RouterModule],
   // standalone:true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
