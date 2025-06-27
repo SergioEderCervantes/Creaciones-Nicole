9c30a6f1fe6b48240caf09c8a33616c3c45fc621
StoneyGravy963 <al210356@edu.uaa.mx>
2025-06-27 01:05:34 -0600

    Home con imagenes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,10 +5,11 @@ import { HomeComponent } from './components/home/home.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { ButtonModule } from 'primeng/button';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,RouterModule, ButtonModule, AdminPanelComponent],
+  imports: [RouterOutlet, NavbarComponent, RouterModule, ButtonModule, AdminPanelComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
