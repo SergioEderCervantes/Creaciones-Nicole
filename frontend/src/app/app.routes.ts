d06938f71e3b47862dbede6b084de9b7e5ad0f83
Yahir D Luna <al335944@edu.uaa.mx>
2025-06-26 19:17:27 -0600

    Fucnionalidad del carrucel en base a los tags

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+import { ReposteriaComponent } from './components/reposteria/reposteria.component';
 
 export const routes: Routes = [
     {path: 'home',component:HomeComponent},
     {path: 'admin',component:AdminPanelComponent},
+    {path: 'reposteria',component:ReposteriaComponent},
 ];
---
+++ frontend/src/app/components/reposteria/reposteria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CarouselComponent } from "../resources/carrucel/carrucel.component";
+
+@Component({
+  selector: 'app-reposteria',
+  imports: [CarouselComponent],
+  templateUrl: './reposteria.component.html',
+  styleUrl: './reposteria.component.css'
+})
+export class ReposteriaComponent {
+
+}
